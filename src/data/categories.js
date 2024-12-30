7ca4d065d91e4c94d1e63bca59169c5e3f4a876d
SipuliCodes <vepsalainensisu@gmail.com>
2024-12-30 18:57:14 +0200

    Change category data location
    Moved category information to its own file for easier up-to-date keeping

---
+++ src/data/categories.js
@@ -0,0 +1,29 @@
+import {
+  faCar,
+  faPaintBrush,
+  faBicycle,
+  faGamepad,
+  faShirt,
+  faTv,
+  faWrench,
+  faMobile,
+  faLaptop,
+  faPuzzlePiece,
+  faUmbrella,
+  faPlus,
+} from '@fortawesome/free-solid-svg-icons';
+
+export const categories = [
+  { id: 'car', icon: faCar, name: 'Cars and accessories' },
+  { id: 'art-tools', icon: faPaintBrush, name: 'Art tools and accessories' },
+  { id: 'bike', icon: faBicycle, name: 'Bikes and accessories' },
+  { id: 'gaming', icon: faGamepad, name: 'Gaming consoles and games' },
+  { id: 'clothes', icon: faShirt, name: 'Clothes' },
+  { id: 'tv', icon: faTv, name: 'Tvs and accessories' },
+  { id: 'tools', icon: faWrench, name: 'Tools' },
+  { id: 'phone', icon: faMobile, name: 'Phones and accessories' },
+  { id: 'laptop', icon: faLaptop, name: 'Laptops and accessories' },
+  { id: 'toys', icon: faPuzzlePiece, name: 'Toys' },
+  { id: 'outdoor', icon: faUmbrella, name: 'Outdoor' },
+  { id: 'others', icon: faPlus, name: 'Others' },
+];
