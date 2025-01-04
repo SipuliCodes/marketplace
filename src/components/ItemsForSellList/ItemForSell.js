99ddf4127a20dfb914794dfae92162e3d308214b
SipuliCodes <vepsalainensisu@gmail.com>
2025-01-04 23:49:17 +0200

    Update product card
    Added small balls at bottom of image to show amount of pics and current pic
    Fixed double click focus bug, picture turned blue on arrow double click

--- src/components/ItemsForSellList/ItemForSell.js
+++ src/components/ItemsForSellList/ItemForSell.js
@@ -17,77 +17,78 @@ const ItemForSell = ({
 
     if (clickedId === "left") {
       if (picNumber > 0) {
-        setPicNumber(picNumber-1)
+        setPicNumber(picNumber - 1)
       }
     }
     if (clickedId === "right") {
       if (picNumber < pics.length - 1) {
-        setPicNumber(picNumber+1);
+        setPicNumber(picNumber + 1);
       }
     }
+    if (clickedId === "bottom") {
+      setPicNumber(parseInt(event.target.dataset.number))
+    }
   }
-
-  if (name === "Toyota Corolla 2020") {
-    return (
-      <div className="flex flex-col rounded-xl bg-white dark:bg-zinc-800 hover:bg-gray-300 dark:hover:bg-zinc-700 shadow-lg  transition-transform transform hover:scale-105 cursor-pointer">
+  return (
+    <div className="flex flex-col rounded-xl bg-white dark:bg-zinc-800 hover:bg-gray-300 dark:hover:bg-zinc-700 shadow-lg  transition-transform transform hover:scale-105 cursor-pointer group">
+      <div className="w-full h-[240px] rounded-xl overflow-hidden relative noselect">
         <Image
-          src={pics[0]}
+          src={pics[picNumber]}
           width="100"
           height="100"
-          className="w-full h-[240px] rounded-xl object-cover overflow-hidden"
+          className="w-full h-full object-cover"
           alt={name}
         />
-        <p className="ml-2">{location}</p>
-        <p className="ml-2 mb-2">
-          {name} | price: {price}€
-        </p>
-      </div>
-    );
-  } else {
-    return (
-      <div className="flex flex-col rounded-xl bg-white dark:bg-zinc-800 hover:bg-gray-300 dark:hover:bg-zinc-700 shadow-lg  transition-transform transform hover:scale-105 cursor-pointer group">
-        <div className="w-full h-[240px] rounded-xl overflow-hidden">
-          <Image
-            src={pics[picNumber]}
-            width="100"
-            height="100"
-            className="w-full h-full object-cover"
-            alt={name}
-          />
-          {picNumber != 0 && (
-            <div
-              id="left"
-              onClick={handleChevronClick}
-              className="flex md:hidden md:group-hover:flex justify-center items-center absolute bg-white/50 hover:bg-white/100 dark:bg-black/50 dark:hover:bg-black/100 rounded-full w-8 h-8 absolute -translate-y-full top-1/2 left-2"
-            >
-              <FontAwesomeIcon
-                icon={faChevronLeft}
-                className="text-blue-800 dark:text-white"
-                size="xl"
+        {picNumber != 0 && (
+          <div
+            id="left"
+            onClick={handleChevronClick}
+            className="flex md:hidden md:group-hover:flex justify-center items-center absolute bg-white/50 hover:bg-white/100 dark:bg-black/50 dark:hover:bg-black/100 rounded-full w-8 h-8 absolute -translate-y-1/2 top-1/2 left-2"
+          >
+            <FontAwesomeIcon
+              icon={faChevronLeft}
+              className="text-blue-800 dark:text-white"
+              size="xl"
+            />
+          </div>
+        )}
+        {picNumber != pics.length - 1 && (
+          <div
+            id="right"
+            onClick={handleChevronClick}
+            className="flex md:hidden md:group-hover:flex justify-center items-center absolute bg-white/50 hover:bg-white/100 dark:bg-black/50 dark:hover:bg-black/100 rounded-full w-8 h-8 absolute -translate-y-1/2 top-1/2 right-2"
+          >
+            <FontAwesomeIcon
+              icon={faChevronRight}
+              className="text-blue-800 dark:text-white"
+              size="xl"
+            />
+          </div>
+        )}
+        {pics.length != 0 && (
+          <div className="flex w-full justify-center absolute bottom-2 space-x-1">
+            {pics.map((_, index) => (
+              <div
+                id="bottom"
+                data-number={index}
+                onClick={handleChevronClick}
+                key={index}
+                className={`w-2 h-2 rounded-full border ${
+                  picNumber !== index
+                    ? 'bg-white hover:bg-gray-300 dark:hover:bg-zinc-700 dark:hover:border-zinc-700'
+                    : 'bg-blue-800 dark:bg-zinc-800'
+                }`}
               />
-            </div>
-          )}
-          {picNumber != pics.length - 1 && (
-            <div
-              id="right"
-              onClick={handleChevronClick}
-              className="flex md:hidden md:group-hover:flex justify-center items-center absolute bg-white/50 hover:bg-white/100 dark:bg-black/50 dark:hover:bg-black/100 rounded-full w-8 h-8 absolute -translate-y-full top-1/2 right-2"
-            >
-              <FontAwesomeIcon
-                icon={faChevronRight}
-                className="text-blue-800 dark:text-white"
-                size="xl"
-              />
-            </div>
-          )}
-        </div>
-        <p className="ml-2">{location}</p>
-        <p className="ml-2 mb-2">
-          {name} | price: {price}€
-        </p>
+            ))}
+          </div>
+        )}
       </div>
-    );
-  }
+      <p className="ml-2">{location}</p>
+      <p className="ml-2 mb-2">
+        {name} | price: {price}€
+      </p>
+    </div>
+  );
 };
 
 export default ItemForSell;
